5eb15de33399e4dddfbbd873b99493548f13d9a6
gilman qazi <gilmanqazi@gmail.com>
2026-02-04 16:15:23 +0530

    frontend integration

--- Day-11/Backend/src/app.js
+++ Day-11/Backend/src/app.js
@@ -3,7 +3,8 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const path = require("path")
-app.use(express.static(path.join(__dirname,"public","index.html")))
+app.use(express.static(path.join(__dirname, "public")));
+
 
 
 
