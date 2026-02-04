030b03f4a63ebfd6011405f9d0e041e71d1f2ad8
gilman qazi <gilmanqazi@gmail.com>
2026-02-04 21:01:53 +0530

    frontend integration

--- Day-11/Backend/server.js
+++ Day-11/Backend/server.js
@@ -48,7 +48,7 @@ app.patch("/api/notes/:id",async (req,res)=>{
   })
 })
 console.log(__dirname)
-app.use("*name/", (req, res) => {
+app.use("*name", (req, res) => {
   res.sendFile(path.join(__dirname,"./public/index.html"));
 });
 
