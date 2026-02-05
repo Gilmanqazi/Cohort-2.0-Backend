86d79475fdb4a59b364495894e44c8ad1fd48b1c
gilman qazi <gilmanqazi@gmail.com>
2026-02-05 19:53:17 +0530

    frontend integration

--- Day-11/Backend/server.js
+++ Day-11/Backend/server.js
@@ -48,10 +48,9 @@ app.patch("/api/notes/:id",async (req,res)=>{
   })
 })
 console.log(__dirname)
-app.use("*name", (req, res) => {
-  res.sendFile(path.join(__dirname,"./public/index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
 
 app.listen(3000,()=>{
   console.log("server is running on 3000");
