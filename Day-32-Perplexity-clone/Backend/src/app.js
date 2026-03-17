2ba5fa988d70805eb3e2d263c5fe59156bbcc4f3
gilman qazi <gilmanqazi@gmail.com>
2026-03-17 13:06:04 +0530

    Backend auth complete

---
+++ Day-32-Perplexity-clone/Backend/src/app.js
@@ -0,0 +1,24 @@
+import express from "express"
+import authRouter from "./Routes/auth.route.js"
+import cookieParser  from "cookie-parser"
+import cors from "cors"
+import morgan from "morgan"
+
+const app = express()
+
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true,
+  methods:["GET", "POST", "PUT", "DELETE"]
+}))
+
+//Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(morgan("dev"))
+
+
+app.use("/api/auth",authRouter)
+    
+export default app
